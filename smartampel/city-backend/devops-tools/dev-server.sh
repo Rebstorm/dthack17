65fe56a939cf5da2452f54ec43a74fa1c987b415
Basti Tee <basti.tee@posteo.de>
2017-09-19 14:43:59 +0200

    gitignore; added startup script;

--- smartampel/city-backend/devops-tools/dev-server.sh
+++ smartampel/city-backend/devops-tools/dev-server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cd "$( dirname "$( readlink -f "$0" )" )"
-[ ! -z $( command -v python3 ) ] && PYCOM="python3" || PYCOM="python"
-$PYCOM manage.py test && $PYCOM manage.py runserver localhost:8000
+
+cd $( dirname $( readlink -f $0 ) )
+cd dthack17
+git pull 
+cd smartampel/city-backend 
+nohup python3 manage.py runserver 63434 &
+ps -ef | grep 63434 | awk '{print $2}' | xargs kill -9 
+
