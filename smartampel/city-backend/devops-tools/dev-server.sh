6be5dd255ffce738ae5d8402c130771f84663616
Basti Tee <basti.tee@posteo.de>
2017-09-19 14:45:55 +0200

    dev-script

--- smartampel/city-backend/devops-tools/dev-server.sh
+++ smartampel/city-backend/devops-tools/dev-server.sh
@@ -4,6 +4,6 @@ cd $( dirname $( readlink -f $0 ) )
 cd dthack17
 git pull 
 cd smartampel/city-backend 
-nohup python3 manage.py runserver 63434 &
 ps -ef | grep 63434 | awk '{print $2}' | xargs kill -9 
+nohup python3 manage.py runserver 63434 &
 
