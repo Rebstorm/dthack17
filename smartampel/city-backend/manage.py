18ed75094ef802aab5cd96be47acf95b10d697fa
Basti Tee <basti.tee@posteo.de>
2017-09-19 18:17:04 +0200

    fixed-managepy

--- smartampel/city-backend/manage.py
+++ smartampel/city-backend/manage.py
@@ -11,23 +11,6 @@ chdir(dname)
 
 if __name__ == "__main__":
 
-    from bptbx import b_iotools
-
     environ.setdefault("DJANGO_SETTINGS_MODULE", "xlight_project.settings")
-
-    import_success, failed_modules = b_iotools.import_modules_with_check(
-        ['django.http'])
-    if not import_success:
-        print('Sorry, but there are some packages missing: '
-              + '{0}\nPlease refer to README.md to find out what '
-              + 'you\'ve missed or, assuming you have \'pip\' installed,\n'
-              + 'run \'pip install -r requirements.txt\''
-              ).format(failed_modules)
-        exit(1)
-
-    # this needs to be done here because why want to check for missing
-    # packages first
     from django.core.management import execute_from_command_line
-
-    # Startup the server
     execute_from_command_line(sys.argv)
