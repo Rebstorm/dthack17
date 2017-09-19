09dc54c4e3df2a54926808b1da6f079e7cabf015
Basti Tee <basti.tee@posteo.de>
2017-09-19 18:14:00 +0200

    updated-dev-script

--- smartampel/city-backend/devops-tools/dev-server.sh
+++ smartampel/city-backend/devops-tools/dev-server.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd $( dirname $( readlink -f $0 ) )
-cd dthack17
-git pull 
-cd smartampel/city-backend 
+cd $( dirname $( readlink -f $0 ) )/../
+echo "-- killing old server" 
 ps -ef | grep 63434 | awk '{print $2}' | xargs kill -9 
+echo "-- firing up"
 nohup python3 manage.py runserver 63434 &
-
+echo "-- done"
